5400ff08c275c0c7f1817db69d84939bffd745e9
571730 <571730@stud.hvl.no>
2017-09-08 12:06:51 +0200

    Ny side, søkefunksjon

---
+++ main.js
@@ -0,0 +1,16 @@
+function finnFag() {
+    var input, filter, ul, li, a, i;
+    input = document.getElementById('sokFag');
+    filter = input.value.toUpperCase();
+    ul = document.getElementById('fagUl');
+    li = ul.getElementsByTagName('li');
+
+    for (i = 0; i < li.length; i++){
+        a = li[i].getElementsByTagName("a")[0];
+        if (a.innerHTML.toUpperCase().indexOf(filter) > -1){
+            li[i].style.display = "";
+        } else {
+            li[i].style.display = "none";
+        }
+    }
+}
